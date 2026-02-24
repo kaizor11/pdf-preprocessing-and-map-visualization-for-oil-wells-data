731b914b957292357143897423d4070abf6c011f
Your Correct Name <your_correct_email@example.com>
2026-02-23 16:10:06 -0800

    updated sql

--- store_sql.py
+++ store_sql.py
@@ -7,12 +7,13 @@ load_dotenv()
 
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = ""
-DB_PORT = 123
-DB_NAME = ""
-TABLE_NAME = ""
+DB_HOST = "100.53.132.91"
+DB_PORT = 3306
+DB_NAME = "oil_wells"
+TABLE_NAME = "well_info_df"
+CSV_FILE = "output.csv"
 
-df = pd.read_csv("output_new.csv")
+df = pd.read_csv()
 
 engine = create_engine(
     f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
